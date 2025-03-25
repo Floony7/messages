c487c92d5c2c0dc7f3f81de1faa16f6759536eba
Floony7 <fred.lunjevich@stretchsense.com>
2025-03-25 07:05:50 +0000

    Implement basic routes

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,28 @@
+import { readFile, writeFile } from 'fs/promises';
+import { CreateMessageDto } from './dtos/create-message.dto';
+
+export class MessagesRepository {
+    async findOne(id: string) {
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
+
+        return messages[id];
+      }
+
+    async findAll() {
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
+    
+        return messages;
+      }
+
+    async create(content: string) {
+        const contents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(contents);
+
+        const id = Math.floor(Math.random() * 999);
+        messages[id] = { id, content };
+
+        writeFile('messages.json', JSON.stringify(messages));
+    }
+}
